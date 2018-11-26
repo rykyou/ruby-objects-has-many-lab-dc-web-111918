3663ea796f865991ee63a0d2c589b0a36e305afa
Robin Kim <rkim1214@gmail.com>
2018-11-26 03:30:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,14 @@ class Artist
     @songs 
   end
   
-  def add_song(song)
-    @songs << song
+  def add_song(song_name)
+    @songs << song_name
     song.artist = self
   end 
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
+    @songs << song
     song.artist = self 
   end 
   
