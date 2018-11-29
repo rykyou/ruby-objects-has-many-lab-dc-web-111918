b7eb8da424e3997425c6f9322530163e18b8e4a3
Robin Kim <rkim1214@gmail.com>
2018-11-29 15:37:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,11 @@
 class Artist 
   attr_accessor :name
+  attr_reader :songs
   
   def initialize(artist_name)
     @name = artist_name 
     @songs = []
   end 
-  
-  def songs 
-    @songs 
-  end
   
   def add_song(song)
     @songs << song
